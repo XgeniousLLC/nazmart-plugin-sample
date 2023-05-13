5824e4c2153bffc098370812bff991b61a349f5b
Sharifur <sharifurrahman@Sharifurs-MacBook-Pro.local>
2023-05-13 16:39:56 +0600

    added enable/disable settings

--- Http/Controllers/SiteWayPaymentGatewayAdminPanelController.php
+++ Http/Controllers/SiteWayPaymentGatewayAdminPanelController.php
@@ -2,6 +2,7 @@
 
 namespace Modules\SiteWayPaymentGateway\Http\Controllers;
 
+use App\Helpers\ModuleMetaData;
 use Illuminate\Http\Request;
 use Illuminate\Routing\Controller;
 
@@ -9,7 +10,14 @@ class SiteWayPaymentGatewayAdminPanelController extends Controller
 {
     public function settings()
     {
-        return  view("sitewaypaymentgateway::admin.settings");
+        $all_module_meta_data = (new ModuleMetaData("SiteWayPaymentGateway"))->getExternalPaymentGateway();
+        $sitesways = array_filter($all_module_meta_data,function ( $item ){
+            if ($item->name === "SitesWay"){
+                return $item;
+            }
+        });
+        $sitesways = current($sitesways);
+        return  view("sitewaypaymentgateway::admin.settings",compact("sitesways"));
     }
 
     public function settingsUpdate(Request $request){
@@ -20,6 +28,12 @@ class SiteWayPaymentGatewayAdminPanelController extends Controller
 
         update_static_option("sitesway_api_key",$request->sitesway_api_key);
         update_static_option("sitesway_brand_id",$request->sitesway_brand_id);
+
+        $jsonModifier = json_decode(file_get_contents("core/Modules/SiteWayPaymentGateway/module.json"));
+        $jsonModifier->nazmartMetaData->paymentGateway->status = $request?->sitesway_status === 'on';
+        $jsonModifier->nazmartMetaData->paymentGateway->admin_settings->show_admin_landlord = $request?->sitesway_landlord_status === 'on';
+        $jsonModifier->nazmartMetaData->paymentGateway->admin_settings->show_admin_tenant = $request?->sitesway_tenant_status === 'on';
+        file_put_contents("core/Modules/SiteWayPaymentGateway/module.json",json_encode($jsonModifier));
 
         return back()->with(["msg" => __("Settings Update"),"type" => "success"]);
     }
--- Resources/views/admin/settings.blade.php
+++ Resources/views/admin/settings.blade.php
@@ -11,6 +11,11 @@
                     @csrf
                     <x-fields.input type="text" value="{{get_static_option('sitesway_api_key')}}" name="sitesway_api_key" label="{{__('Api Key')}}"/>
                     <x-fields.input type="text" value="{{get_static_option('sitesway_brand_id')}}" name="sitesway_brand_id" label="{{__('Brand Id')}}"/>
+
+                    <x-fields.switcher label="{{__('Sitesway Enable/Disable Landlord & Tenant Both')}}" name="sitesway_status" value="{{$sitesways->status}}"/>
+                    <x-fields.switcher label="{{__('Sitesway Enable/Disable Landlord Websites')}}" name="sitesway_landlord_status" value="{{$sitesways->admin_settings->show_admin_landlord}}"/>
+                    <x-fields.switcher label="{{__('Sitesway Enable/Disable Tenant Websites')}}" name="sitesway_tenant_status" value="{{$sitesways->admin_settings->show_admin_tenant}}"/>
+
                     <button type="submit" class="btn btn-gradient-primary mt-5 me-2">{{__('Save Changes')}}</button>
                 </form>
             </div>
